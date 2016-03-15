e0b9d12d1cd4d0f51b7cc5294479ebde50b33cdc
Louis Lainé <darksioul6@gmail.com>
2016-03-15 12:50:59 +0100

    Init deps and config

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  entry: getEntrySources(['./src/index.js']),
+  output: {
+    publicPath: 'http://localhost:8080/',
+    filename: 'build/bundle.js'
+  },
+  devtool: 'eval',
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'source-map'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'url?limit=8192',
+          'img'
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loaders: ['babel']
+      }
+    ]
+  }
+};
+
+function getEntrySources(sources) {
+  if (process.env.NODE_ENV !== 'production') {
+    sources.push('webpack-dev-server/client?http://localhost:8080');
+    sources.push('webpack/hot/only-dev-server');
+  }
+
+  return sources;
+}
